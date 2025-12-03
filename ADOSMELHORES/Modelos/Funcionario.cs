08174059ed6f5dc5a33806e79270c6abacf881ae
D4rKing <rui.santos.maia@gmail.com>
2025-12-03 19:58:38 +0000

    Added Funcionario

---
+++ ADOSMELHORES/Modelos/Funcionario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOSMELHORES
+{
+    internal abstract class Funcionário
+    {
+        public int Id { get; set; }
+        public int Nif { get; set; }
+        public string Nome { get; set; }
+        public string Morada { get; set; }
+        public string Contacto { get; set; }
+        public DateTime DataFimContrato { get; set; }
+        public DateTime DataFimRegistoCrim { get; set; }
+    }
+}
