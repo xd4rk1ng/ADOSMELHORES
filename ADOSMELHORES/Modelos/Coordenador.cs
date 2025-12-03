1dce43038877ed9d39345833b36064ccc7363ee1
diegomayrinck <157934919+diegomayrinck@users.noreply.github.com>
2025-12-03 22:31:50 +0000

    Criação do coordenador
    Criação da classe coordenador dos formadores. Cada coordenador terá uma lista de formadores.

--- ADOSMELHORES/Modelos/Coordenador.cs
+++ ADOSMELHORES/Modelos/Coordenador.cs
@@ -8,5 +8,43 @@ namespace ADOSMELHORES.Modelos
 {
     internal class Coordenador : Funcionario
     {
+        public List<Formador> FormadoresAssociados { get; set; }
+        public string AreaCoordenacao { get; set; }
+
+        public Coordenador()
+        {
+            FormadoresAssociados = new List<Formador>();
+        }
+
+        //construtor com parametros
+        public Coordenador(int id, int nif, string nome, string morada, string contacto, DateTime dataFimContrato, DateTime dataIniContrato, DateTime dataFimRegistoCrim, decimal salarioBase,
+            DateTime dataNascimento, string areaCoordenacao)
+            : base(id, nif, nome, morada, contacto, dataFimContrato, dataIniContrato, dataFimRegistoCrim, dataNascimento, salarioBase)
+        {
+            AreaCoordenacao = areaCoordenacao;
+            FormadoresAssociados = new List<Formador>();
+        }
+
+        public void AdicionarFormador(Formador formador)
+        {
+            if (!FormadoresAssociados.Contains(formador))
+            {
+                FormadoresAssociados.Add(formador);
+            }
+        }
+        public void RemoverFormador(Formador formador)
+        {
+            FormadoresAssociados.Remove(formador);
+        }
+        public override decimal CalcularCustoMensal()
+        {
+            return SalarioBase;
+        }
+        public int NumeroFormadores => FormadoresAssociados.Count;
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} - {AreaCoordenacao} - {NumeroFormadores} formadores";
+        }
     }
 }
