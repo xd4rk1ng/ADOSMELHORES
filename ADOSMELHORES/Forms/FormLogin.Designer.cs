cfac665bd89f65fd940bea93d2e8586cfb04dabd
talits-prog <talita.calaca@gmail.com>
2026-01-30 12:21:09 +0000

    mudança no form Login

--- ADOSMELHORES/Forms/FormLogin.Designer.cs
+++ ADOSMELHORES/Forms/FormLogin.Designer.cs
@@ -37,29 +37,33 @@
             // lblTitulo
             // 
             this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitulo.Location = new System.Drawing.Point(13, 10);
+            this.lblTitulo.Location = new System.Drawing.Point(17, 15);
+            this.lblTitulo.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(283, 80);
+            this.lblTitulo.Size = new System.Drawing.Size(377, 122);
             this.lblTitulo.TabIndex = 0;
-            this.lblTitulo.Text = "Bem vindo ao ADOSMELHORES";
+            this.lblTitulo.Text = "Bem vindo ao ADosMelhores\r\n";
             this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // lblPassword
             // 
             this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.lblPassword.Location = new System.Drawing.Point(19, 109);
+            this.lblPassword.Location = new System.Drawing.Point(25, 167);
             this.lblPassword.Margin = new System.Windows.Forms.Padding(0);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(277, 17);
+            this.lblPassword.Size = new System.Drawing.Size(369, 26);
             this.lblPassword.TabIndex = 0;
             this.lblPassword.Text = "Password de Administrador";
             this.lblPassword.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // btnEntrar
             // 
-            this.btnEntrar.Location = new System.Drawing.Point(77, 168);
+            this.btnEntrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEntrar.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrar.Location = new System.Drawing.Point(103, 259);
+            this.btnEntrar.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.btnEntrar.Name = "btnEntrar";
-            this.btnEntrar.Size = new System.Drawing.Size(160, 30);
+            this.btnEntrar.Size = new System.Drawing.Size(213, 46);
             this.btnEntrar.TabIndex = 1;
             this.btnEntrar.Text = "Entrar";
             this.btnEntrar.UseVisualStyleBackColor = true;
@@ -67,26 +71,31 @@
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(77, 129);
+            this.txtPassword.Location = new System.Drawing.Point(103, 199);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(160, 20);
+            this.txtPassword.Size = new System.Drawing.Size(212, 27);
             this.txtPassword.TabIndex = 1;
             // 
             // FormLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(309, 261);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(252)))), ((int)(((byte)(249)))));
+            this.ClientSize = new System.Drawing.Size(409, 388);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.btnEntrar);
             this.Controls.Add(this.lblTitulo);
             this.Controls.Add(this.lblPassword);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(100)))), ((int)(((byte)(82)))));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MaximumSize = new System.Drawing.Size(325, 300);
-            this.MinimumSize = new System.Drawing.Size(325, 300);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.MaximumSize = new System.Drawing.Size(427, 435);
+            this.MinimumSize = new System.Drawing.Size(427, 435);
             this.Name = "FormLogin";
-            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Padding = new System.Windows.Forms.Padding(13, 15, 13, 15);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             this.ResumeLayout(false);
